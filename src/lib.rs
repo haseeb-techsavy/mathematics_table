c141db4e9cb8d9bfba3f7fe58d59451f7cefcb4a
Haseeb ul Hassan <haseeb.ee12@gmail.com>
2020-05-11 11:47:29 +0500

    Changed the code in lib.rs and adding license Apache and license Mit files

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,8 @@
 pub mod table {
-    pub fn integer () {
-        println!("Please enter a number to generate its table");
-        let mut user_input = String::new();   
-        std::io::stdin().read_line(&mut user_input).unwrap(); //Reading the user input
-        println!("You entered: {}", user_input);
-        let user_input:u32 = user_input.trim().parse().     //Converting string in an integer
-        expect("Enter a valid number");
-        println!("The table of {} is as follow:\n", user_input);
+    pub fn integer (number:usize) {
+        println!("The table of {} is as follow:\n", number);
         for count in 1..=10 {
-            println!("{} X {} = {}", user_input, count, user_input * count);
+            println!("{} X {} = {}", number, count, number * count);
         }
-
     }
 }
